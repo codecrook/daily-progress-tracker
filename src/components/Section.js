1c17758358ca52a08ff9631f2067fab88f714f8a
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-04-25 23:33:59 +0530

    ADD: WeekendTitle by extending WeekdayTitle and conditionally render it inside Section

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,29 +2,40 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledSection = styled.div`
-  background-color: #ffffff;
-  border: solid 3px grey;
-  margin: 4px;
-  width: 300px;
-  height: 50px;
-  border-radius: 10px;
-  display: flex;
-  justify-content: left;
-  font-size: 40px;
-  color: #ff6961;
-`
+    background-color: #ffffff;
+    border: solid 3px grey;
+    margin: 4px;
+    width: 300px;
+    height: 50px;
+    border-radius: 10px;
+    display: flex;
+    justify-content: left;
+    font-size: 40px;
+    color: #ff6961;
+`;
 
 const WeekdayTitle = styled.div`
-  display: flex;
-  justify-content: center;
-  width: 50px;
-  border-right: solid 3px lightgrey;
-`
+    display: flex;
+    justify-content: center;
+    width: 50px;
+    border-right: solid 3px lightgrey;
+`;
+
+const WeekendTitle = styled(WeekdayTitle)`
+    background-color: lightgrey;
+    border-radius: 7px 0 0 7px;
+`;
 
 export const Section = ({ text }) => {
+
+    const isWeekend = text === 'S';
     return (
         <StyledSection>
-            <WeekdayTitle>{text}</WeekdayTitle>
+            {
+                isWeekend ?
+                    <WeekendTitle>{text}</WeekendTitle> :
+                    <WeekdayTitle>{text}</WeekdayTitle>
+            }
         </StyledSection>
     )
 }
