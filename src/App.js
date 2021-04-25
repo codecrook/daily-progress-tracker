02f4b2d528b2f075ee1ee317c000f888bf17c3d5
Deeptiman Mallick <deeptiman_mallick@outlook.com>
2021-04-25 22:56:35 +0530

    ADD: Section and WeekdayTitle styled-components to create base tracker layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
-import './App.css';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: #b19cd9;
+`;
+
+const Section = styled.div`
+  background-color: #ffffff;
+  border: solid 3px grey;
+  margin: 4px;
+  width: 300px;
+  height: 50px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: left;
+  font-size: 40px;
+  color: #ff6961;
+`;
+
+const WeekdayTitle = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 50px;
+  border-right: solid 3px lightgrey;
+`;
 
 function App() {
   return (
-    <div>
-      <h1>Daily Progress Tracker</h1>
-    </div>
+    <>
+      <Title>Daily Progress Tracker</Title>
+      <div>
+        <Section>
+          <WeekdayTitle>M</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>T</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>W</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>T</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>F</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>S</WeekdayTitle>
+        </Section>
+        <Section>
+          <WeekdayTitle>S</WeekdayTitle>
+        </Section>
+      </div>
+    </>
   );
 }
 
